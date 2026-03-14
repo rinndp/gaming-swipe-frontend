032e5ed74635a260223301d7406b63a804fe7c9a
axelPortatil <axelperez221105@gmail.com>
2026-03-14 20:25:36 +0100

    improving IGDB api token management

--- app/data/sources/remote/igdbAPI/IgdbApiDelivery.ts
+++ app/data/sources/remote/igdbAPI/IgdbApiDelivery.ts
@@ -1,13 +1,23 @@
 import axios from "axios";
-import { IGDB_CLIENT_ID, IGDB_AUTH } from '@env';
+import { IGDB_CLIENT_ID } from '@env';
+import { ApiDelivery } from "../api/ApiDelivery";
+
+let cachedToken: string | null = null;
 
 const IgdbApiDelivery = axios.create({
     baseURL: "https://api.igdb.com/v4/",
     headers: {
         "Content-Type": "text/plain",
         "Client-ID": IGDB_CLIENT_ID,
-        "Authorization": "Bearer " + IGDB_AUTH,
     },
-})
+});
 
+IgdbApiDelivery.interceptors.request.use(async (config) => {
+    if (!cachedToken) {
+        const response = await ApiDelivery.get('igdb/token');
+        cachedToken = response.data.token;
+    }
+    config.headers.Authorization = "Bearer " + cachedToken;
+    return config;
+});
 export { IgdbApiDelivery };
